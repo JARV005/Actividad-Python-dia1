1b2c9cce4ee357dad1f8bb5ae2ab473607c925b5
JARV005 <johanalexanderriveravasquez@gmail.com>
2025-04-21 21:50:38 -0500

    Update Ej10.py

--- Ej10.py
+++ Ej10.py
@@ -1,4 +1,8 @@
 x=input("Ingrese un número del 1 al 10 ")
 x=int(x)
-for x in range(1,x):
-    print(x)
+if 1<=x<=10:
+    x=str(x)
+    print("Su número es "+x)
+else:
+    print("Su número no esta en el rango ")
+    
