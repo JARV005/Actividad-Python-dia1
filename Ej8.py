ccf4ba2139ace889c48ab6c472ded1932241d6cf
JARV005 <johanalexanderriveravasquez@gmail.com>
2025-04-21 21:41:15 -0500

    Update Ej8.py

--- Ej8.py
+++ Ej8.py
@@ -8,10 +8,10 @@ imc=(x/(y*y))
 if imc<18.5:
     print("Bajo peso")
 
-if 18.5>=imc<=24.9:
+if 18.5<=imc<=24.9:
     print("Normal")
 
-if 25>=imc<=29.9:
+if 25<=imc<=29.9:
     print("Sobrepeso") 
 
 if imc>=30:
